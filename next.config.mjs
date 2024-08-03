5049439739daddab5162e5d0fbd1570311d3cfe5
Gabriel Brun <lydstyl@gmail.com>
2024-08-03 15:01:57 +0200

    Fix missingSuspenseWithCSRBailout issue in build.

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   //       '/api/downloadDocx': ['./tmp/**/*']
   //     }
   //   }
+  experimental: {
+    missingSuspenseWithCSRBailout: false
+  }
 }
 
 export default nextConfig
