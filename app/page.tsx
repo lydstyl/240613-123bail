af41d8c8c8095dfaa7603f5ceebfdbb015f50a70
Gabriel Brun <lydstyl@gmail.com>
2024-08-17 16:02:08 +0200

    Install and use react-hook-form.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
-import React, { useState, FormEvent } from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import OrderPreviewButton from '@/components/OrderPreviewButton'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  tenantName: string
+  lessorName: string
+}
 
 type Props = {
   // leaseUrl: string | null
@@ -16,28 +22,34 @@ const Home: React.FC<Props> = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<Inputs>()
+
+  const onSubmit2: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+
     setIsLoading(true)
     setError(null) // Clear previous errors when a new request starts
 
     try {
-      const formData = new FormData(event.currentTarget)
-
-      const response = await fetch('/api/patchLease', {
+      const response: Response = await fetch('/api/patchLease', {
         method: 'POST',
         headers: {
           Accept: 'application/json'
           // 'Content-Type': 'application/json'
         },
         // body: Object.fromEntries(formData)
-        body: JSON.stringify(Object.fromEntries(formData))
+        body: JSON.stringify(data)
       })
       if (!response.ok) {
         throw new Error('Failed to submit the data. Please try again.')
       }
-      const data = await response.json()
-      setLeaseUrl(data.url)
+      const data2 = await response.json()
+      setLeaseUrl(data2.url)
       // // in local storage
       // if (typeof window !== 'undefined') {
       //   console.log('localStorage', data.url)
@@ -51,36 +63,33 @@ const Home: React.FC<Props> = () => {
     }
   }
 
+  console.log(watch('tenantName')) // watch input value by passing the name of it
+
   return (
     <main className='p-24'>
       <h1 className='text-2xl'>123 bail !</h1>
-
+      <hr />
       {!leaseUrl2 && (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit2)}>
           <input
-            className='m-10 bg-slate-300'
-            type='text'
-            name='tenantName'
-            placeholder='Nom du locataire'
+            defaultValue='tenantName'
+            {...(register('tenantName'), { required: true })}
           />
-          <input
-            className='m-10 bg-slate-300'
-            type='text'
-            name='lessorName'
-            placeholder='Nom du bailleur'
-          />
+          {errors.tenantName && <span>tenantName est requis</span>}
 
-          <button className='bg-slate-200' type='submit'>
-            Générer le bail
-          </button>
+          <input
+            defaultValue='lessorName'
+            {...register('lessorName', { required: true })}
+          />
+          {errors.lessorName && <span>lessorName est requis</span>}
+
+          <input type='submit' />
         </form>
       )}
-
+      <hr />
       {leaseUrl && <OrderPreviewButton leaseUrl={leaseUrl} />}
       {leaseUrl2 && <a href={leaseUrl2}>Télécharger le bail</a>}
-
       {leaseUrl && <a href={leaseUrl}>Remove this download dev button</a>}
-      {/* mettre tout ça sur une autre page pour que la home soit une home de SEO avec video et argument marketing */}
     </main>
   )
 }
