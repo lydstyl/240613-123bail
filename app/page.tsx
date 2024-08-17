c9b3b480befd2f47d00525f588588cb9a1a5f57d
Gabriel Brun <lydstyl@gmail.com>
2024-08-17 16:53:34 +0200

    Install and use zod to validate form and handle errors.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@
 import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import OrderPreviewButton from '@/components/OrderPreviewButton'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
-type Inputs = {
-  tenantName: string
-  lessorName: string
-}
+const mySchema2 = z.object({
+  tenantName: z.string().min(1, { message: 'Required !!' }),
+  lessorName: z.string().min(1, { message: 'Required yé' })
+})
 
+type Inputs = z.infer<typeof mySchema2>
 type Props = {
   // leaseUrl: string | null
 }
@@ -21,18 +24,19 @@ const Home: React.FC<Props> = () => {
   const [leaseUrl, setLeaseUrl] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
-
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm<Inputs>()
+  } = useForm({
+    resolver: zodResolver(mySchema2)
+  })
 
-  const onSubmit2: SubmitHandler<Inputs> = async (data) => {
+  const onSubmit = async (data: any) => {
     try {
       console.log(data)
       setIsLoading(true)
-      setError(null) // Clear previous errors when a new request starts
+      setError(null)
       const response: Response = await fetch('/api/patchLease', {
         method: 'POST',
         headers: {
@@ -40,7 +44,6 @@ const Home: React.FC<Props> = () => {
           // 'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-        // body: data
       })
       if (!response.ok) {
         throw new Error('Failed to submit the data. Please try again.')
@@ -67,14 +70,16 @@ const Home: React.FC<Props> = () => {
       <h1 className='text-2xl'>123 bail !</h1>
       <hr />
       {!leaseUrl2 && (
-        <form onSubmit={handleSubmit(onSubmit2)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input defaultValue='tenantName' {...register('tenantName')} />
 
           <input
             defaultValue='lessorName'
             {...register('lessorName', { required: true })}
           />
-          {errors.lessorName && <span>lessorName est requis</span>}
+          {errors.lessorName?.message && (
+            <p>{errors.lessorName?.message?.toString()}</p>
+          )}
 
           <input type='submit' />
         </form>
